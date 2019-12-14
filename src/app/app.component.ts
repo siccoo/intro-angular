655a125808a55b430fc6948774cd0c85b38238d4
siccoo <obichilaka@gmail.com>
2019-12-14 22:31:41 +0100

    Modifications of some files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 function log(target, name, descriptor) {
   console.log(target, name, descriptor);
-  
+  // Store the original function in a variable
+  const original = descriptor.value
+  // Do some manipulations in the descriptor.value which is the function
+  descriptor.value = function() {
+    console.log("Hi Michael, Kudos!!!"); 
+  }
+  // Return the descriptor
+  return descriptor
 }
 
 @Component({
@@ -12,6 +19,10 @@ function log(target, name, descriptor) {
 })
 export class AppComponent {
   title = 'intro2angular';
+
+  constructor() {
+    this.aSimpleMethod()
+  }
 
   @log aSimpleMethod() {
     console.log("Hi Nicholas, I'm learning Angular!");
