3d72c25f2adce986912f5ffcc50c414b9b2a4f16
siccoo <obichilaka@gmail.com>
2019-12-21 17:46:53 +0100

    Update on some files in hello folder

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -8,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class HelloComponent implements OnInit {
 
   myvariable = "Moya, Nich & Michael";
-  bitwiseOR = 2 | 5 
-  // 010 101 => 0 | 1 = 1 then 1 | 1 = 0 then 1 | 0 = 1; 111
+  bitwiseOR = 2 | 5 // 010 101 => 0 | 1 = 1 then 1 | 1 = 0 then 1 | 0 = 1; 111
+  i = 9;
 
   constructor() { }
+
+  doSomeHeavyTask() {
+    console.log(`Called ${this.i++} times`);
+    
+  }
 
   ngOnInit() {
   }
