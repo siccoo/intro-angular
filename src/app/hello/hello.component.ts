e8ed5d7f386350bb8295831d4bf71bdf57cec131
siccoo <obichilaka@gmail.com>
2019-12-21 17:00:41 +0100

    Added a new css file to the hello component

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hello',
   templateUrl: './hello.component.html',
-  styleUrls: ['./hello.component.scss']
+  styleUrls: ['./hello.component.css']
 })
 export class HelloComponent implements OnInit {
 
