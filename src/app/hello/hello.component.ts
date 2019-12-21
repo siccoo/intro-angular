d91277acbf3853dbd3bdad6ad84341d955003f14
siccoo <obichilaka@gmail.com>
2019-12-21 17:38:53 +0100

    Understanding the syntax of Angular

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloComponent implements OnInit {
 
+  myvariable = "Moya, Nich & Michael";
+  bitwiseOR = 2 | 5 
+  // 010 101 => 0 | 1 = 1 then 1 | 1 = 0 then 1 | 0 = 1; 111
+
   constructor() { }
 
   ngOnInit() {
